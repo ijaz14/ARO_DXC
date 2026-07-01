3fea500a880c62b2d5792fd0597abdec0aaae263
Ijaz <ijaz14@gmail.com>
2026-07-02 06:16:42 +0930

    updated login.sh

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,14 +1,22 @@
+# #!/bin/bash
+
+# set -e
+
+# echo "Logging into Azure..."
+
+# az login --service-principal \
+#   -u "$AZURE_CLIENT_ID" \
+#   -p "$AZURE_CLIENT_SECRET" \
+#   --tenant "$AZURE_TENANT_ID"
+
+# az account set --subscription "$AZURE_SUBSCRIPTION_ID"
+
+# echo "Login successful"
+
 #!/bin/bash
 
 set -e
 
-echo "Logging into Azure..."
-
-az login --service-principal \
-  -u "$AZURE_CLIENT_ID" \
-  -p "$AZURE_CLIENT_SECRET" \
-  --tenant "$AZURE_TENANT_ID"
-
-az account set --subscription "$AZURE_SUBSCRIPTION_ID"
-
+echo "Azure login already handled by AzureCLI@2"
 echo "Login successful"
+
